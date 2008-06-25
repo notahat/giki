33d4ff3c8fc6ee43276b524c3498f1ab3245b0af
Pete Yandell <pete@notahat.com>
2008-06-25 20:19:18 +1000

    Added rough version of ability to pull from and push to remotes for a project.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -16,4 +16,21 @@ class ProjectsController < ApplicationController
     flash[:notice] = "Project deleted."
     redirect_to(projects_url)
   end
+  
+  def pull
+    @project = Project.find(params[:id])
+    @remote = params[:remote]
+    @project.git.fetch(@remote)
+    @project.git.merge("#{@remote}/master")
+    flash[:notice] = "Pull completed."
+    redirect_to(project_pages_url(@project))
+  end
+  
+  def push
+    @project = Project.find(params[:id])
+    @remote = params[:remote]
+    @project.git.push(@remote)
+    flash[:notice] = "Push completed."
+    redirect_to(project_pages_url(@project))
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :projects do |project|
+  map.resources :projects, :member => { :pull => :get, :push => :get } do |project|
     project.resources :pages, :new => { :new => :any }
   end
   map.root :projects
